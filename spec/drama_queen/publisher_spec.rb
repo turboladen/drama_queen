97b1f1241c12d3bbf166274c580d9acc8a8c2ed3
Steve Loveless <steve.loveless@gmail.com>
2013-11-05 23:08:30 -0800

    Added publisher specs

--- spec/drama_queen/publisher_spec.rb
+++ spec/drama_queen/publisher_spec.rb
@@ -1,4 +1,5 @@
 require_relative '../spec_helper'
+require 'minitest/mock'
 require 'drama_queen/publisher'
 
 
@@ -8,8 +9,40 @@ describe DramaQueen::Publisher do
   end
 
   describe '#publish with no subscribers' do
+    before do
+      DramaQueen.subscribers.stub(:has_key?, false) do
+        false
+      end
+    end
+
     it 'returns nil' do
       subject.publish('test').must_equal nil
+    end
+  end
+
+  describe '#publish with subscribers of a different topic' do
+    before do
+      DramaQueen.subscribers[:another_topic] = true
+    end
+
+    it 'returns nil' do
+      subject.publish('test').must_equal nil
+    end
+  end
+
+  describe '#publish with subscribers of a same topic' do
+    let(:subscriber) { MiniTest::Mock.new }
+
+    before do
+      DramaQueen.subscribers['test'] = [{
+        subscriber: subscriber,
+        callback: :test_method
+      }]
+    end
+
+    it 'calls the callback' do
+      subscriber.expect :send, true, [:test_method, []]
+      subject.publish('test')
     end
   end
 end
