039d7ec1e8ac51f8c972b80947196e0840351e2c
Steve Loveless <steve.loveless@gmail.com>
2013-11-08 13:26:22 -0800

    Bump to rspec 3

--- spec/drama_queen/consumer_spec.rb
+++ spec/drama_queen/consumer_spec.rb
@@ -11,7 +11,7 @@ describe DramaQueen::Consumer do
     it 'adds itself to the list of subscribers' do
       callback = double 'Method', call: true
 
-      subject.should_receive(:method).with(:call_me) { callback }
+      expect(subject).to receive(:method).with(:call_me) { callback }
       subject.subscribe('test', :call_me)
 
       expect(DramaQueen.subscribers.size).to eq 1
