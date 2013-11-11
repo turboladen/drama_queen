3c95031ab8d36afb49ba8fac4699c6cf70be16ad
Steve Loveless <steve.loveless@gmail.com>
2013-11-11 09:47:11 -0800

    New test

--- test.rb
+++ test.rb
@@ -1,57 +1,47 @@
 =begin
-require './lib/drama_queen/publisher'
+require './lib/drama_queen/producer'
+require './lib/drama_queen/consumer'
 
 class A
-  include DramaQueen::Publisher
-
-  def do_stuff
-    publish 'stuff1', "I'm doing the stuff111111111"
-    publish 'stuff2', "I'm doing the stuff222222222"
-    publish 'stuff5', "I'm doing the stuff555555555"
-    publish 'stuff3', "I'm doing the stuff333333333"
-    publish 'stuff5', "I'm doing the stuff555555555"
-  end
-end
-
-
-require './lib/drama_queen/subscriber'
-
-class B
-  include DramaQueen::Subscriber
+  include DramaQueen::Consumer
 
   def initialize
-    100000.times do
-      subscribe 'stuff1', :call_me
-      subscribe 'stuff2', :call_me2
-      subscribe 'stuff3', :call_me
-      subscribe 'stuff4', :call_me2
-      subscribe 'stuff5', ->(arg) {
-        #puts arg
-      }
-    end
+    subscribe 'root.*.children', :call_me
   end
 
-  def call_me(msg)
-    puts msg
-  end
-
-  def call_me2(msg)
-    puts msg
+  def call_me(*args)
+    puts "A got called with args: #{args}"
   end
 end
+
+class B
+  include DramaQueen::Producer
+
+  def do_stuff
+    publish 'root.parent', 1, 2, 3
+  end
+end
+
+class C
+  include DramaQueen::Producer
+
+  def do_stuff
+    publish 'root.parent.children', 1, 2, 3
+  end
+end
+
 
 a = A.new
 b = B.new
+c = C.new
 
-a.do_stuff
-
-b.subscribe 'things', ->(arg) { puts arg }
-
-a.do_stuff
-puts 'did stuff'
-a.publish 'things', 'hi'
+puts 'B...'
+b.do_stuff
+puts 'C...'
+c.do_stuff
 =end
 
+=begin
 require './lib/drama_queen/producer'
 
 class A
@@ -83,40 +73,91 @@ a = A.new
 b = B.new
 
 a.do_stuff
+=end
+
+
+def routes? first, second
+  first_minus_second = first - second
+  second_minus_first = second - first
+  puts "first: #{first}"
+  puts "second: #{second}"
+  puts "first minus second: #{first - second}"
+  puts "second minus first: #{second - first}"
+  puts "first size: #{first.size}"
+  puts "second size: #{second.size}"
+
+  puts 'first loop'
+  first.each_cons(second.size) { |e| p e }
+  puts 'second loop'
+  second.each_cons(first.size) { |e| p e }
+
+  if first == second
+    true
+  elsif (first_minus_second).uniq == %w[*] && first.size == second.size
+    true
+  elsif first_minus_second.all? { |e| e =~ Regexp.new(second_minus_first.join('.'))}
+    true
+=begin
+  elsif (second_minus_first).size == (first_minus_second).size &&
+    first.size == second.size &&
+    first_minus_second =~ second_minus_first
+    true
+=end
+  else
+    false
+  end
+end
+
+a = %w[root parent child]
+b = %w[root * child]
+c = %w[root parent meow]
+d = %w[root parent *]
+e = %w[root * child *]
+f = %w[root parent child thing]
+
+puts '-' * 40
+puts '-' * 40
+puts 'A' * 40
+puts "a should route to a: #{routes? a, a}"
+puts '-' * 40
+
+puts "a should route to b: #{routes? a, b}"
+puts '-' * 40
+
+puts "a should NOT route to c: #{routes? a, c}"
+puts '-' * 40
+
+puts "a should route to d: #{routes? a, d}"
+puts '-' * 40
+
+puts "a should NOT route to e: #{routes? a, e}"
+puts '-' * 40
+
+puts "a should NOT route to f: #{routes? a, f}"
+puts '-' * 40
 
 =begin
-class A
-  include DramaQueen::Publisher
+puts '-' * 40
+puts '-' * 40
+puts 'B' * 40
+puts "b should route to a: #{routes? b, a}"
+puts '-' * 40
 
-  def do_stuff
-    publish 'stuff', "I'm doing the stuff"
-  end
-end
+puts "b should route to b: #{routes? b, b}"
+puts '-' * 40
 
+puts "b should NOT route to c: #{routes? b, c}"
+puts '-' * 40
 
-require './lib/drama_queen/subscriber'
+puts "b should route to d: #{routes? b, d}"
+puts '-' * 40
 
-class B
-  include DramaQueen::Subscriber
+puts "b should NOT route to e: #{routes? b, e}"
+puts '-' * 40
 
-  def initialize
-    1000000.times do
-      #100.times do
-      subscribe 'stuff', :call_me
-    end
-  end
+puts "b should NOT route to f: #{routes? b, f}"
+puts '-' * 40
 
-  def call_me(msg)
-    #puts msg
-  end
-
-  def call_me2(msg)
-    #puts msg
-  end
-end
-
-a = A.new
-b = B.new
-
-a.do_stuff
+puts "c should NOT route to a: #{routes? c, a}"
+puts '-' * 40
 =end
